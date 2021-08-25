30d6cac8ce6aac0b6fb7a69282a76a737464557b
JaishalPatel <80263421+JaishalPatel@users.noreply.github.com>
2021-08-25 18:31:31 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "MSDHONI";
+const accessCode2 = "LORDS";
+const accessCode3 = "JOEROOT";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("H D I O N S M", 100,50)
+    fill("lightblue")
+    text("Hint: Known as captain cool", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("O R D L S", 700,150)
+    fill("lightblue")
+    text("Hint: Known as home of cricket!!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("O T O R E O J", 100,250)
+    fill("lightblue")
+    text("Hint: Captain of England test squad !!", 100,270)
+
+}
